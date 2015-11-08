d9d78fa2d76e02f3d4ee6d41774ca76edb907930
Luisa Morales <me@luisam.com>
2015-11-07 22:50:41 -0500

    Initial project setup

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # technica15
 Technica Hack Project
+
+Requirements:
+* Python 2.7 - http://www.python.org
+* pip - http://pip.readthedocs.org/en/latest
+* virtualenv - http://virtualenv.readthedocs.org/en/latest
+* Flask 
+
+Setup Steps: 
+* Pip (see below in Notes)
+* Once pip is installed, in terminal run: sudo pip install virtualenv
+* Create virtual environment, run: virtualenv --no-site-packages venv
+* Start virtual environment, run: 
+	- For Mac / Unix: source venv/bin/activate
+	- For Windows: source bin/activate
+* Install Flask, run: pip install flask
+* Start Development Server, run: python app.py
+	- Open a new browser window and go to: localhost:5000
+
+
+Notes:
+* Most recent version of pip (as of Nov 15) is included in project directory
+	- If you don't have pip installed, open terminal, cd into your project directory and run: python get-pip.py
+	- If you get a permission error, run: sudo python get-pip.py
+
---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template("index.html")
+
+
+@app.route('/schools')
+def schools():
+	return render_template("schools.html")
+
+@app.route('/school-results')
+def school_results():
+	return render_template("school-results.html")
+
+@app.route('/scholarships')
+def scholarships():
+	return render_template("scholarships.html")
+
+@app.route('/loans')
+def loans():
+	return render_template("loans.html")
+
+@app.route('/login')
+def login():
+	return render_template("login.html")
+
+@app.route('/profile')
+def profile():
+	return render_template("profile.html")
+
+@app.route('/profile/account-settings')
+def account_settings():
+	return render_template("account-settings.html")
+
+@app.route('/profile/background')
+def background():
+	return render_template("background.html")
+
+@app.route('/profile/fafsa')
+def fafsa():
+	return render_template("fafsa.html")
+
+@app.route('/profile/friendfund')
+def friendfund():
+	return render_template("friendfund.html")
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
